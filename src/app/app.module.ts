f08c0d0fc2dac7b7a665e8cd736282f5b7ffd857
rstar2 <rstar2@abv.bg>
2017-01-13 17:05:18 +0200

    Added all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
 import { appStateProvider } from "./store/app-store";
+
+import { AppComponent } from './app.component';
 import { ChatPageComponent } from "./components/page.component";
 import { ChatWindowComponent } from "./components/window.component";
 import { ChatMessageComponent } from "./components/message.component";
 import { ChatNavBarComponent } from "./components/nav-bar.component";
 import { ChatThreadsComponent } from "./components/threads.component";
 import { ChatThreadComponent } from "./components/thread.component";
+
 import { FromNowPipe } from "./pipes/from-now.pipe";
 
 @NgModule({
